e219a3af94f19253b22725e417a5d0c3633b1e2e
taylor-stinson <taylor.stinson@my.jcu.edu.au>
2020-08-23 20:13:27 +1000

    Create function for determining rank of score

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,13 +3,28 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
+
+def main():
     score = float(input("Enter score: "))
-if score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    rank = determine_rank(score)
+    print(rank)
+    import random
+    score = random.randrange(1, 101, 1)
+    rank = determine_rank(score)
+    print(rank)
+
+
+def determine_rank(score):
+    if score < 50:
+        rank = "Bad"
+    elif score < 90:
+        rank = "Passable"
+    else:
+        rank = "Excellent"
+    return rank
+
+
+main()
