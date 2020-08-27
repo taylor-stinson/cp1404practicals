ade9d461c8ea906a580efe899a8f73ad61bc61d9
taylor-stinson <taylor.stinson@my.jcu.edu.au>
2020-08-27 15:11:10 +1000

    Complete warmup

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,28 @@
+"""
+CP1404 Practicals
+Lists Warm-up
+"""
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# numbers[0]
+# = 3
+# numbers[-1]
+# = 2
+# numbers[3]
+# = 1
+# numbers[:-1]
+# = 3, 1, 4, 1, 5, 9, 2 # wrong- 2 not included
+# numbers[3:4]
+# = 1, 5 # wrong- 5 not included
+# 5 in numbers
+# = True
+# 7 in numbers
+# = False
+# "3" in numbers
+# = False
+# numbers + [6, 5, 3]
+# = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+# 1. numbers[0] = "ten"
+# 2. numbers[-1] = 1
+# 3. numbers[:1:-1]
+# 4. 9 in numbers
+
