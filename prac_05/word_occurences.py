6d87a131f43f70b8cd52b6fcfa72b82e9f6d7bde
taylor-stinson <taylor.stinson@my.jcu.edu.au>
2020-09-10 16:08:42 +1000

    Added word_occurences.py

---
+++ prac_05/word_occurences.py
@@ -0,0 +1,36 @@
+"""
+CP1404 - Practicals
+Word Occurrences
+"""
+
+
+def main():
+    word_count = create_sorted_dictionary()
+    longest_word = determine_longest_string(word_count)
+    for key, value in word_count.items():
+        print(f"{key:<{longest_word}}:{value}")
+
+
+def create_sorted_dictionary():
+    word_count = {}
+    text = str(input("Text: "))
+    words = text.split()
+    words.sort()
+    for word in words:
+        if word not in word_count:
+            word_count[word] = 1
+        else:
+            word_count[word] += 1
+    return word_count
+
+
+def determine_longest_string(string):
+    """Determines length of the longest string in a list"""
+    longest_string_length = 0
+    for word in string:
+        if len(word) > longest_string_length:
+            longest_string_length = len(word)
+    return longest_string_length
+
+
+main()
