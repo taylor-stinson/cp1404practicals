b9f5efcc62cec7949855b20716ee4550323c58f3
taylor-stinson <taylor.stinson@my.jcu.edu.au>
2020-09-26 01:49:48 +1000

    Write guitar.py and guitar_test.py

---
+++ prac_06/guitar.py
@@ -0,0 +1,20 @@
+"""
+CP1404 Practical - Guitar Class
+"""
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost}"
+
+    def get_age(self):
+        age = 2020 - self.year
+        return age
+
+    def is_vintage(self):
+        return self.get_age() >= 50
