076ec2f7b3e3fe57cd331b6163f2e2360bb5be69
taylor-stinson <taylor.stinson@my.jcu.edu.au>
2020-09-26 01:17:02 +1000

    Complete languages.py and programming_language.py

---
+++ prac_06/languages.py
@@ -0,0 +1,14 @@
+"""
+CP1404 Practical - Languages
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+languages = [ruby, python, visual_basic]
+print("The dynamically typed languages are: ")
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,5 +2,16 @@
 
 
 class ProgrammingLanguage:
-    """"""
-   def __init__(self):
+    """Store information about a programming language"""
+    def __init__(self, name, typing, reflection, year):
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+        self.name = name
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        if self.typing == "Dynamic":
+            return True
